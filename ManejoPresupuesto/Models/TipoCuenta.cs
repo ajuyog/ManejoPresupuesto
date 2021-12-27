9696b9b856a05bdea8f7db7d522e3c012d396cf6
ajuyo <ajuyo@LAPTOP-3A4INKG2>
2021-12-27 16:54:19 -0500

    Agregar archivos de proyecto.

---
+++ ManejoPresupuesto/Models/TipoCuenta.cs
@@ -0,0 +1,15 @@
+﻿using ManejoPresupuesto.Validaciones;
+using System.ComponentModel.DataAnnotations;
+
+namespace ManejoPresupuesto.Models
+{
+    public class TipoCuenta
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="El camnpo {0} es requerido")]
+        [PrimeraLetraMayuscula]
+        public string Nombre { get; set; }
+        public int UsuarioId { get; set; }
+        public int Orden { get; set; }
+    }
+}
